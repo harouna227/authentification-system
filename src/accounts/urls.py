9725fd94492877d7893798ae8c1c2942586b7cb7
Harouna227 <issifiharouna07@gmail.com>
2024-02-25 00:19:51 +0100

    my last commit

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -18,6 +18,4 @@ urlpatterns = [
     path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path("password-reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path("password-reset/compete/", auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    
-    
 ]
